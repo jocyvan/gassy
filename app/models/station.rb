250dda218275242e4bc2686533e5c32a9d2a4b30
Jocvan Castro <jocyvan.castro@gmail.com>
2015-09-06 19:04:30 -0300

    Follower counter

--- app/models/station.rb
+++ app/models/station.rb
@@ -9,4 +9,5 @@ class Station < ActiveRecord::Base
   has_many :rates, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :follows, dependent: :destroy
+  has_many :followers, through: :follows, source: :user
 end
