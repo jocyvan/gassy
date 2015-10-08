122efeddf555e6d13affcebf1de310c1bd001b17
Jocyvan Castro <jocyvan@Mac-mini-de-Jocyvan.local>
2015-10-08 11:31:13 -0300

    Fix likes counter on station show

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -4,7 +4,6 @@ class Rate < ActiveRecord::Base
   belongs_to :station, counter_cache: true
   belongs_to :user
 
-  scope :likes, -> { where(status: :like) }
   scope :this_month, -> { where(created_at: Date.today.at_beginning_of_month..Date.today.at_end_of_month) }
   scope :prev_month, -> { where(created_at: Date.today.prev_month.at_beginning_of_month..Date.today.prev_month.at_end_of_month) }
   scope :prev_2_month, -> { where(created_at: Date.today.prev_month.prev_month.at_beginning_of_month..Date.today.prev_month.prev_month.at_end_of_month) }
