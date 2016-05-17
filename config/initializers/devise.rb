5204435fbaae6c0e6a7977ed56ef88d82866066d
Jocyvan Castro <jocyvan.castro@gmail.com>
2016-05-17 10:17:35 -0300

    Redefining Facebook Key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,7 +236,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "1187336311290404", "73a895c6dbc75b0daa292d1f8de76e38", scope: 'email', info_fields: 'email,name'
+  config.omniauth :facebook, "1187336311290404", ENV['FB_KEY'], scope: 'email', info_fields: 'email,name'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
